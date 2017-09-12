bff588f8c47e09f2f37c6e2af29544c7528d561a
Irvel <irvel.nduva@gmail.com>
2017-09-12 01:02:33 -0500

    Update outline draft for the preprocessing module

--- pre_process_files.py
+++ pre_process_files.py
@@ -4,5 +4,6 @@ Preprocess Module
 - Normalizes the Sampling rate
 - Normalizes volume levels
 - Trims files that are too long
+- Trim beginning and end of files
 - Augments dataset by generating combinations of speech noise
 """
